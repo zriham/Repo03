9a16d68c3a5264c7b9b0daa419ac2437cae04e02
zriham <mahir.zolota@live.com>
2021-09-15 18:09:47 +0200

    Dodao vjezbu 2 i dodao uzvicnik u vjezbu hello world!

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,6 +1,6 @@
 function main() {
 // Only change code below this line
-console.log('Hello World');
+console.log('Hello World!');
 // Only change code above this line
 }
 main();
---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+// Only change code below this line
+
+// This is my one line comment
+
+/* This is
+my
+multi-line
+comment */
+
+// Only change code above this line
+}
+console.log(main());
+module.exports=main;
