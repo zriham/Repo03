cc4fc9afb95fd1d1a886e8cb2392383a201ac362
zriham <mahir.zolota@live.com>
2021-09-15 18:37:39 +0200

    Dodao vjezbu ex04 sa kreiranim variablama

---
+++ ex04/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+// Only change code below this line
+var x;
+var y;
+
+x=10;
+y=x;
+// Only change code below this line
+return [x, y];
+
+}
+console.log(main());
+module.exports = main;
